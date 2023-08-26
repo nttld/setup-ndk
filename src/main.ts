bfca17f5e677510d2f22ea92d9ca234ab83ab07d
Raphaël Thériault <self@raftar.io>
2023-08-26 12:54:09 -0700

    big revamp

--- src/main.ts
+++ src/main.ts
@@ -1,22 +1,21 @@
-import * as core from '@actions/core'
-import {getNdk} from './installer'
+import * as core from "@actions/core"
 
-async function run(): Promise<void> {
-  try {
-    const version = core.getInput('ndk-version')
-    const addToPath = core.getBooleanInput('add-to-path')
-    const localCache = core.getBooleanInput('local-cache')
-    const path = await getNdk(version, addToPath, localCache)
-    core.setOutput('ndk-path', path)
-  } catch (error) {
-    core.setFailed(asError(error))
-  }
+import { getNdk } from "./installer"
+
+async function main() {
+  const version = core.getInput("ndk-version")
+  const addToPath = core.getBooleanInput("add-to-path")
+  const localCache = core.getBooleanInput("local-cache")
+  const path = await getNdk(version, addToPath, localCache)
+  core.setOutput("ndk-path", path)
 }
 
 function asError(error: unknown): Error | string {
-  if (typeof error === 'string') return error
+  if (typeof error === "string") return error
   else if (error instanceof Error) return error
-  else return `${error}`
+  else return Object.prototype.toString.call(error)
 }
 
-run()
+main().catch((error) => {
+  core.setFailed(asError(error))
+})
